996808ac9a8528f55676057aac1ab014cf107116
PAUCHOU <PAUCHOU@Kevin-Chous-MacBook-Pro.local>
2014-09-16 16:12:57 -0700

    trie lab

--- trie.js
+++ trie.js
@@ -1,4 +1,3 @@
-
 Trie = function(){
   this.characters = {};
 };
@@ -7,8 +6,21 @@ Trie.prototype.learn = function(word, index){
   // This function should add the given word,
   // starting from the given index,
   // to this Trie.
+   if(index === undefined) {
+    index = 0;
+  }
+  if (word[index] !== undefined) {
+    if (this.characters[word[index]] === undefined) {
+    this.characters[word[index]] = new Trie();
+    this.characters[word[index]].learn(word, index+1);
+    } else {
+    this.characters[word[index]].learn(word, index+1);
+    }
+  } else {
+    this.isWord = true;
+  }
 
-  // It will be recursive.  It will tell
+   // It will be recursive.  It will tell
   // the correct child of this Trie to learn the word
   // starting from a later index.
 
@@ -24,23 +36,62 @@ Trie.prototype.getWords = function(words, currentWord){
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
+    if (words === undefined){
+      words = [];
+    }
+    if(currentWord === undefined){
+      currentWord = "";
+    }
+
+    if(this.isWord){
+      words.push(currentWord);
+    }
+
+  for(var letter in this.characters){
+    this.characters[letter].getWords(words,currentWord + letter);
+  }
+
+  return words;
 };
 
 Trie.prototype.find = function(word, index){
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
+  if(index === undefined){
+    index =0;
+  }
 
+  if (this.characters[word[index]] === undefined){
+    return false;
+  } else {
+      if (index === (word.length-1)){
+      return this.characters[word[index]];
+      } else {
+        return this.characters[word[index]].find(word, index+1);
+      }
+  }
   // Be sure to consider what happens if the word is not in this Trie.
 };
 
 Trie.prototype.autoComplete = function(prefix){
-  // This function will return all completions 
+  // This function will return all completions
   // for a given prefix.
   // It should use find and getWords.
+var auto = this.find(prefix);
+var array = [];
+
+if (auto){
+  var results = auto.getWords();
+  for (var i = 0; i < results.length; i += 1){
+    array.push(prefix+results[i]);
+  }
+}
+return array;
+
 };
 
 try{
-  module.exports = Trie
+  module.exports = Trie;
 } catch(e){
 
 }
